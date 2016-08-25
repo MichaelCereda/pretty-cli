8f067e476322d7db3b401a2be83c299a25c2c567
Marvin Hagemeister <marvin.hagemeister@sevenval.com>
2016-08-25 22:48:52 +0200

    Fix undefined template error

--- src/Pretty.js
+++ src/Pretty.js
@@ -12,11 +12,11 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 
 import {union} from 'lodash';
 
-class ExternalInterface{
-  constructor(opts){
+class ExternalInterface {
+  constructor(opts) {
     this.settings = {};
 
-    Object.assign(this.settings, this.settings, opts||{});
+    Object.assign(this.settings, this.settings, opts || {});
 
     this._output = opts.output || console.log;
     this._context = opts.context || {};
@@ -25,89 +25,84 @@ class ExternalInterface{
     this._printFn = opts.print;
     this._cached_messages = [];
 
-    let methods = union([ 'log',
-      'info',
-      'warning',
-      'success',
-      'error'
-    ], Object.keys(opts.template))
-    methods.map((method, i)=>{
-      this[method] = ((contentType)=>{
+    let methods = ['log', 'info', 'warning', 'success', 'error'];
+
+    if (opts.template) {
+      methods = union(methods, Object.keys(opts.template));
+    }
+
+    methods.map((method, i) => {
+      this[method] = ((contentType) => {
         var _ct = contentType;
-        return (content)=>{
-          if(typeof content !== 'string'){
+        return (content) => {
+          if (typeof content !== 'string') {
             Object.assign(content, this._context, content);
           }
           this._output(content, _ct);
-      }
-    })(method)
+        }
+      })(method)
     })
 
-    if(this._parent_key !==''){
-      this['clear'] = ()=>{
+    if (this._parent_key !== '') {
+      this['clear'] = () => {
         this._stack = {};
       }
-      this['get'] = ()=>{
+      this['get'] = () => {
         return this._stack;
       }
-      this['print'] = ()=>{
-        // console.log(this.settings)
+      this['print'] = () => {
         this._printFn()
       }
     }
   }
 
-  stack(key){
-    let newSettings = {};
+  stack(key) {
+    const newSettings = Object.assign({}, this.settings, {
+      stack: {},
+      parent_key: key
+    });
 
-    Object.assign(
-      newSettings,
-      this.settings,
-      {stack:{},parent_key:key})
-      this.stack[key] = new ExternalInterface(newSettings);
+    this.stack[key] = new ExternalInterface(newSettings);
     return this.stack[key];
   }
-  context(ctx){
+
+  context(ctx) {
     this._context = ctx;
     return this;
   }
-  addCustomMethod(name, fn){
+  addCustomMethod(name, fn) {
     this[name] = fn;
   }
-
-
 }
 
-
-module.exports = function(opts){
+module.exports = function (opts) {
   opts = opts || {}
 
-  let output = (opts)=>{
+  let output = opts => {
     var _opts = opts;
 
-    return (content, contentType)=>{
+    return (content, contentType) => {
       var tmpl;
-      if(!_opts || !_opts.template || !_opts.template[contentType]){
-        tmpl = x=>x;
+      if (!_opts || !_opts.template || !_opts.template[contentType]) {
+        tmpl = x => x;
       } else {
         tmpl = _opts.template[contentType]
       }
-      if(!content) content = '';
+      if (!content) content = '';
 
       let str = tmpl(content);
 
-      if(_opts.printOutput===false){
+      if (_opts.printOutput === false) {
         return str;
       }
       console.log(str)
     }
   }
   Object.assign(opts, {
-    output:output
-  },opts)
+    output: output
+  }, opts)
   // Inizializing output function
   opts.output = opts.output(opts)
   opts.print = opts.output
-  // console.log(opts);
   return new ExternalInterface(opts)
 }
